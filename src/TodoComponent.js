7531c7c6df29fbb0a37530bd2bfc67bd33cba207
Stefano Corra <stefano.corra@hotmail.com>
2017-08-27 08:25:00 -0400

    second commit

--- src/TodoComponent.js
+++ src/TodoComponent.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem'
+
 class TodoComponent extends Component {
 
   constructor(props){
-    super();
+    super(props);
+    this.onDelete = this.onDelete.bind(this);
     this.state= {todos:['wash up','eat some cheese', 'take a nap','buy flowers'] }
   }
 
-  render() {
-
+  render(){
       var todos = this.state.todos;
+
       todos = todos.map(function(item,index){
         return (
           <TodoItem item={item} key={index} onDelete={this.onDelete}/>
         );
-      });
+      }.bind(this));
 
     return (
       <div className="todo-list">
@@ -26,10 +28,12 @@ class TodoComponent extends Component {
 
   //Custom Functions
   onDelete(item){
-    var updatedTodos = this.state.todos.filter(function(val,index){
-      return item !== val;
-    });
-    this.setState({ todo: updatedTodos });
+      var updatedTodos = this.state.todos.filter(function(val, index){
+          return item !== val;
+      });
+      this.setState({
+        todos: updatedTodos
+      });
   }
 
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,16 @@ import React, { Component } from 'react';
 
 class TodoItem extends Component {
 
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  
   render(){
     return (
       <li >
         <div className="todo-item">
-          <span className="item-name">{this.props.item}</span>
+          <span className="item-name" ref="thisItem">{this.props.item}</span>
           <span className="item-delete" onClick={this.handleDelete}> x </span>
         </div>
       </li>
